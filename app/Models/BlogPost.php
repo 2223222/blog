84ef262014987096b31ad58a698e5ae23d13b2ce
2223222 <mykhailo.kozak@oa.edu.ua>
2023-05-12 10:59:16 +0300

    lab3

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class BlogPost extends Model
-{use SoftDeletes;
+{
     protected $fillable
         = [
             'title',
@@ -43,4 +44,5 @@ class BlogPost extends Model
     }
 
     use HasFactory;
+    use SoftDeletes;
 }
