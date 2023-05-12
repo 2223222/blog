d1586ebc1e15df871371639bfead2c07b27b9595
2223222 <mykhailo.kozak@oa.edu.ua>
2023-05-12 10:26:02 +0300

    lab1

---
+++ app/Http/Controllers/Blog/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{/**
+ * BaseController constructor
+ */
+    public function __construct()
+    {
+        //Ініціалізація загальних елементів адмінки
+    }
+    //
+}
